064166447ee796e185d7ece05b7d63c49b6597c6
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2022-10-06 15:29:01 -0500

    skeleton code added

---
+++ src/components/ComponentContainer.js
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap"
+export const ComponentContainer = () => {
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <canvas></canvas>
+                </Col>
+            </Row>
+        </Container>
+
+    )
+}

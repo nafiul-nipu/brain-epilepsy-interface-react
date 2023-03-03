84ed14bf95ced49f046311a5e086c0f098b26290
nafiulnipu <mnipu2@uic.edu>
2023-03-02 18:34:42 -0600

    everything together

--- src/components/ElectrodeNetworkTumor.js
+++ src/components/ElectrodeNetworkTumor.js
@@ -102,19 +102,19 @@ export const ElectrodeNetworkTumor = ({
                 transparency: true
             });
 
-            // for (let lesion of lesions) {
-            //     // console.log(lesion);
-            //     //load lesion
-            //     await OBJLoaderThreeJS({
-            //         scene: scene,
-            //         obj: lesion,
-            //         color: 0Xf7680f,
-            //         opacity: 1,
-            //         transparency: false,
-            //         center: false
-            //     });
+            for (let lesion of lesions) {
+                // console.log(lesion);
+                //load lesion
+                await OBJLoaderThreeJS({
+                    scene: scene,
+                    obj: lesion,
+                    color: 0Xf7680f,
+                    opacity: 1,
+                    transparency: false,
+                    center: false
+                });
 
-            // }
+            }
 
             // load electrode and electrode network
             await loadElectrode([scene, scene2], electrodeData, sampleData);
@@ -298,197 +298,197 @@ export const ElectrodeNetworkTumor = ({
 
 
 
-            // let uniforms = {
+            let uniforms = {
 
-            //     pointTexture: { value: new THREE.TextureLoader().load(circle) }
+                pointTexture: { value: new THREE.TextureLoader().load(circle) }
 
-            // };
+            };
 
-            // const shaderMaterial = new THREE.ShaderMaterial({
+            const shaderMaterial = new THREE.ShaderMaterial({
 
-            //     uniforms: uniforms,
-            //     vertexShader: vertexShader,
-            //     fragmentShader: fragmentShader,
-            //     // blending: THREE.AdditiveBlending,
-            //     depthTest: false,
-            //     transparent: true,
-            //     vertexColors: true,
-            //     alphaTest: 0.5,
-            //     side: THREE.DoubleSide,
+                uniforms: uniforms,
+                vertexShader: vertexShader,
+                fragmentShader: fragmentShader,
+                // blending: THREE.AdditiveBlending,
+                depthTest: false,
+                transparent: true,
+                vertexColors: true,
+                alphaTest: 0.5,
+                side: THREE.DoubleSide,
 
-            // });
-
-
-            // let vertices = []
-            // let colors = []
-            // let sizes = []
-            // let colIdx = 0;
-            // const color = new THREE.Color();
-            // let firstColor = []
-            // let firstSize = []
-            // let pointGeometry = new THREE.BufferGeometry();
-            // // add the vertices, need to loop once as positio will be same 
-            // for (let top = 0; top < electrodeData.length; top++) {
-            //     vertices.push(electrodeData[top].position[0], electrodeData[top].position[1], electrodeData[top].position[2])
-            //     let c = colorD3(electrodeData[top].label)
-            //     let d = new THREE.Color(c)
-            //     color.setRGB(10 / 255, 10 / 255, 10 / 255);
-            //     // color.setRGB(253 / 255, 180 / 255, 98 / 255);
-            //     // firstColor.push(color.r, color.g, color.b);
-            //     firstColor.push(d.r, d.g, d.b);
-            //     firstSize.push(6);
-
-            // }
-            // colors.push(firstColor)
-            // sizes.push(firstSize)
-            // // 10 * (1 + Math.sin(0.1 * top + 1))
-
-            // // 55,126,184
-            // sampleData.forEach(data => {
-            //     // do nothing
-            //     let sortedData = structuredClone(data) //
-            //     sortedData.sort((a, b) => b.frequency - a.frequency);
-
-            //     // console.log(Math.round(sortedData.length * percent))
-            //     let startElec = [...new Set(sortedData.slice(0, Math.round(sortedData.length)).map(item => item.start))]
-
-            //     let endElec = [...new Set(sortedData.slice(0, Math.round(sortedData.length)).map(item => item.end))]
-
-            //     // loop through the data 
-            //     let eachColor = []
-            //     let eachSize = []
-
-            //     for (let top = 0; top < electrodeData.length; top++) {
-            //         if (startElec.includes(electrodeData[top].electrode_number)) {
-            //             // start electrode
-            //             // console.log('start')
-            //             color.setRGB(3 / 255, 218 / 255, 197 / 255);
-            //             eachColor.push(color.r, color.g, color.b)
-
-            //             const arr = sortedData.find(p => p.start === electrodeData[top].electrode_number);
-            //             eachSize.push(sizeScale(arr.frequency))
-
-            //         } else if (endElec.includes(electrodeData[top].electrode_number)) {
-            //             // end electrode
-            //             // color.setRGB(10 / 255, 166 / 255, 2 / 255);
-            //             color.setRGB(3 / 255, 218 / 255, 197 / 255);
-            //             eachColor.push(color.r, color.g, color.b);
-
-            //             const arr = sortedData.find(p => p.end === electrodeData[top].electrode_number);
-            //             eachSize.push(sizeScale(arr.frequency))
-            //         } else {
-            //             // rest electrode
-            //             // color.setRGB(253 / 255, 180 / 255, 98 / 255);
-            //             color.setRGB(10 / 255, 10 / 255, 10 / 255);
-            //             eachColor.push(color.r, color.g, color.b);
-            //             eachSize.push(6);
-            //         }
-            //     }
-            //     colors.push(eachColor)
-            //     sizes.push(eachSize)
-
-            // })
+            });
 
 
-            // // console.log(sizes)
-            // pointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-            // pointGeometry.setAttribute('color', new THREE.Float32BufferAttribute(firstColor, 3));
-            // pointGeometry.setAttribute('size', new THREE.Float32BufferAttribute(firstSize, 1).setUsage(THREE.DynamicDrawUsage));
+            let vertices = []
+            let colors = []
+            let sizes = []
+            let colIdx = 0;
+            const color = new THREE.Color();
+            let firstColor = []
+            let firstSize = []
+            let pointGeometry = new THREE.BufferGeometry();
+            // add the vertices, need to loop once as positio will be same 
+            for (let top = 0; top < electrodeData.length; top++) {
+                vertices.push(electrodeData[top].position[0], electrodeData[top].position[1], electrodeData[top].position[2])
+                let c = colorD3(electrodeData[top].label)
+                let d = new THREE.Color(c)
+                color.setRGB(10 / 255, 10 / 255, 10 / 255);
+                // color.setRGB(253 / 255, 180 / 255, 98 / 255);
+                // firstColor.push(color.r, color.g, color.b);
+                firstColor.push(d.r, d.g, d.b);
+                firstSize.push(6);
 
-            // let points = new THREE.Points(pointGeometry, shaderMaterial);
-            // points.geometry.translate(centerOther.x, centerOther.y, centerOther.z);
+            }
+            colors.push(firstColor)
+            sizes.push(firstSize)
+            // 10 * (1 + Math.sin(0.1 * top + 1))
 
-            // scene[1].add(points);
+            // 55,126,184
+            sampleData.forEach(data => {
+                // do nothing
+                let sortedData = structuredClone(data) //
+                sortedData.sort((a, b) => b.frequency - a.frequency);
 
+                // console.log(Math.round(sortedData.length * percent))
+                let startElec = [...new Set(sortedData.slice(0, Math.round(sortedData.length)).map(item => item.start))]
 
-            // // console.log(points)
-            // // change the colours, one a second
-            // inter = setInterval(function () {
+                let endElec = [...new Set(sortedData.slice(0, Math.round(sortedData.length)).map(item => item.end))]
 
-            //     let value = d3.select('#play-pause-btn').property('value')
+                // loop through the data 
+                let eachColor = []
+                let eachSize = []
 
-            //     if (value === 'pause') {
-            //         scene[1].remove(points)
-            //         // console.log("inter")
-            //         colIdx = (colIdx + 1) % colors.length;
+                for (let top = 0; top < electrodeData.length; top++) {
+                    if (startElec.includes(electrodeData[top].electrode_number)) {
+                        // start electrode
+                        // console.log('start')
+                        color.setRGB(3 / 255, 218 / 255, 197 / 255);
+                        eachColor.push(color.r, color.g, color.b)
 
-            //         d3.selectAll('.highlightRect').style('opacity', '0')
-            //         if (colIdx !== 0) {
-            //             d3.selectAll(`#high${colIdx}`).style('opacity', '0.5')
-            //         }
+                        const arr = sortedData.find(p => p.start === electrodeData[top].electrode_number);
+                        eachSize.push(sizeScale(arr.frequency))
 
-            //         if (colIdx === 0) {
-            //             sliderObj.value([0, 0]);
-            //         } else {
-            //             sliderObj.value([(colIdx - 1) * timeRange, colIdx * timeRange]);
-            //         }
+                    } else if (endElec.includes(electrodeData[top].electrode_number)) {
+                        // end electrode
+                        // color.setRGB(10 / 255, 166 / 255, 2 / 255);
+                        color.setRGB(3 / 255, 218 / 255, 197 / 255);
+                        eachColor.push(color.r, color.g, color.b);
 
+                        const arr = sortedData.find(p => p.end === electrodeData[top].electrode_number);
+                        eachSize.push(sizeScale(arr.frequency))
+                    } else {
+                        // rest electrode
+                        // color.setRGB(253 / 255, 180 / 255, 98 / 255);
+                        color.setRGB(10 / 255, 10 / 255, 10 / 255);
+                        eachColor.push(color.r, color.g, color.b);
+                        eachSize.push(6);
+                    }
+                }
+                colors.push(eachColor)
+                sizes.push(eachSize)
 
-            //         // console.log(sizes[colIdx])
-            //         let geometry = new THREE.BufferGeometry();
-            //         geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-            //         geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors[colIdx], 3));
-            //         // points.geometry.colors.set(new THREE.Float32BufferAttribute(colors[colIdx]));
-            //         geometry.setAttribute('size', new THREE.Float32BufferAttribute(sizes[colIdx], 1).setUsage(THREE.DynamicDrawUsage));
-
-            //         points = new THREE.Points(geometry, shaderMaterial);
-            //         points.geometry.colorsNeedUpdate = true;
-            //         points.geometry.translate(centerOther.x, centerOther.y, centerOther.z);
-
-            //         scene[1].add(points);
-
-            //         render(renderer, [scene[0], scene[1]], camera)
-
-            //     } else if (value === 'play' && document.getElementsByClassName('referenceCircle')[0].id !== 'null') {
-            //         const element = document.getElementsByClassName('referenceCircle')
-            //         // console.log(element[0].id)
-            //         let i = +element[0].id;
-            //         scene[1].remove(points)
-
-            //         // console.log(electrodeData)
-            //         // console.log(eventData)
-
-            //         let EEachColor = []
-            //         let EEachSize = []
-            //         for (let top = 0; top < electrodeData.length; top++) {
-            //             if (eventData[i].electrode.includes(electrodeData[top].electrode_number)) {
-            //                 // start electrode
-            //                 console.log('start')
-            //                 color.setRGB(3 / 255, 218 / 255, 197 / 255);
-            //                 EEachColor.push(color.r, color.g, color.b)
-            //                 EEachSize.push(6)
-
-            //             } else {
-            //                 // rest electrode
-            //                 color.setRGB(10 / 255, 10 / 255, 10 / 255);
-            //                 EEachColor.push(color.r, color.g, color.b);
-            //                 EEachSize.push(6);
-            //             }
-            //         }
+            })
 
 
+            // console.log(sizes)
+            pointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+            pointGeometry.setAttribute('color', new THREE.Float32BufferAttribute(firstColor, 3));
+            pointGeometry.setAttribute('size', new THREE.Float32BufferAttribute(firstSize, 1).setUsage(THREE.DynamicDrawUsage));
 
-            //         // console.log(sizes[colIdx])
-            //         console.log(EEachColor)
-            //         console.log(colors)
-            //         let geometry = new THREE.BufferGeometry();
-            //         geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-            //         geometry.setAttribute('color', new THREE.Float32BufferAttribute(EEachColor, 3));
-            //         // points.geometry.colors.set(new THREE.Float32BufferAttribute(colors[colIdx]));
-            //         geometry.setAttribute('size', new THREE.Float32BufferAttribute(EEachSize, 1).setUsage(THREE.DynamicDrawUsage));
+            let points = new THREE.Points(pointGeometry, shaderMaterial);
+            points.geometry.translate(centerOther.x, centerOther.y, centerOther.z);
 
-            //         points = new THREE.Points(geometry, shaderMaterial);
-            //         points.geometry.colorsNeedUpdate = true;
-            //         points.geometry.translate(centerOther.x, centerOther.y, centerOther.z);
-
-            //         scene[1].add(points);
-
-            //         render(renderer, [scene[0], scene[1]], camera)
+            scene[1].add(points);
 
 
-            //         element[0].id = 'null';
-            //     }
-            // }, 2500);
+            // console.log(points)
+            // change the colours, one a second
+            inter = setInterval(function () {
+
+                let value = d3.select('#play-pause-btn').property('value')
+
+                if (value === 'pause') {
+                    scene[1].remove(points)
+                    // console.log("inter")
+                    colIdx = (colIdx + 1) % colors.length;
+
+                    d3.selectAll('.highlightRect').style('opacity', '0')
+                    if (colIdx !== 0) {
+                        d3.selectAll(`#high${colIdx}`).style('opacity', '0.5')
+                    }
+
+                    if (colIdx === 0) {
+                        sliderObj.value([0, 0]);
+                    } else {
+                        sliderObj.value([(colIdx - 1) * timeRange, colIdx * timeRange]);
+                    }
+
+
+                    // console.log(sizes[colIdx])
+                    let geometry = new THREE.BufferGeometry();
+                    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+                    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors[colIdx], 3));
+                    // points.geometry.colors.set(new THREE.Float32BufferAttribute(colors[colIdx]));
+                    geometry.setAttribute('size', new THREE.Float32BufferAttribute(sizes[colIdx], 1).setUsage(THREE.DynamicDrawUsage));
+
+                    points = new THREE.Points(geometry, shaderMaterial);
+                    points.geometry.colorsNeedUpdate = true;
+                    points.geometry.translate(centerOther.x, centerOther.y, centerOther.z);
+
+                    scene[1].add(points);
+
+                    render(renderer, [scene[0], scene[1]], camera)
+
+                } else if (value === 'play' && document.getElementsByClassName('referenceCircle')[0].id !== 'null') {
+                    const element = document.getElementsByClassName('referenceCircle')
+                    // console.log(element[0].id)
+                    let i = +element[0].id;
+                    scene[1].remove(points)
+
+                    // console.log(electrodeData)
+                    // console.log(eventData)
+
+                    let EEachColor = []
+                    let EEachSize = []
+                    for (let top = 0; top < electrodeData.length; top++) {
+                        if (eventData[i].electrode.includes(electrodeData[top].electrode_number)) {
+                            // start electrode
+                            console.log('start')
+                            color.setRGB(3 / 255, 218 / 255, 197 / 255);
+                            EEachColor.push(color.r, color.g, color.b)
+                            EEachSize.push(6)
+
+                        } else {
+                            // rest electrode
+                            color.setRGB(10 / 255, 10 / 255, 10 / 255);
+                            EEachColor.push(color.r, color.g, color.b);
+                            EEachSize.push(6);
+                        }
+                    }
+
+
+
+                    // console.log(sizes[colIdx])
+                    console.log(EEachColor)
+                    console.log(colors)
+                    let geometry = new THREE.BufferGeometry();
+                    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+                    geometry.setAttribute('color', new THREE.Float32BufferAttribute(EEachColor, 3));
+                    // points.geometry.colors.set(new THREE.Float32BufferAttribute(colors[colIdx]));
+                    geometry.setAttribute('size', new THREE.Float32BufferAttribute(EEachSize, 1).setUsage(THREE.DynamicDrawUsage));
+
+                    points = new THREE.Points(geometry, shaderMaterial);
+                    points.geometry.colorsNeedUpdate = true;
+                    points.geometry.translate(centerOther.x, centerOther.y, centerOther.z);
+
+                    scene[1].add(points);
+
+                    render(renderer, [scene[0], scene[1]], camera)
+
+
+                    element[0].id = 'null';
+                }
+            }, 2500);
 
         }
 

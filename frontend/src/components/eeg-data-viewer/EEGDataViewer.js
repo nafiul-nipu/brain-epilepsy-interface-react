76064dbb8edaee2ba848461a593f0b19da284b19
nafiulnipu <mnipu2@uic.edu>
2023-04-16 03:37:35 -0500

    eeg spike panel removed

--- frontend/src/components/eeg-data-viewer/EEGDataViewer.js
+++ frontend/src/components/eeg-data-viewer/EEGDataViewer.js
@@ -1,61 +1,9 @@
 import "./eeg-data-viewer.css";
-import { useState } from "react";
 import { EEGImage } from './eeg-image';
-import Form from "react-bootstrap/Form";
-import { Col, Row, InputGroup, Button, FormControl } from "react-bootstrap";
-
 
 export const EEGDataViewer = ({ eegEL, patientInfo }) => {
-  const [spikeAlgorithm, setSpikeAlgorithm] = useState("bandpass");
-
-  function onSpikeAlgorithmChange(e) {
-    setSpikeAlgorithm(e.target.value);
-  }
-
-  const [inputValue, setInputValue] = useState(1000);
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  }
-
-  const handleButtonOnClick = () => {
-    // console.log(spikeAlgorithm, inputValue)
-    // setBaseUrl(`https://raw.githubusercontent.com/nafiul-nipu/brain-epilepsy-interface-react/eegSpikeDemo/frontend/src/data/${spikeAlgorithm}`);
-  }
-
-
   return (
     <div className="eeg-container">
-      <Row>
-        <Col md='6'>
-          <Form.Group controlId="formHorizontal" className="flex-form-entry">
-            <Form.Label id="selectPosition">Spike_Alg:</Form.Label>
-            <Form.Select
-              value={spikeAlgorithm}
-              id="selectPosition"
-              onChange={onSpikeAlgorithmChange}
-            >
-              <option value="bandpass"> Bandpass </option>
-              <option value="localMaxima"> Local Maxima </option>
-              <option value="wavelt"> Wavelet </option>
-            </Form.Select>
-          </Form.Group>
-        </Col>
-        <Col md='6'>
-          <InputGroup size="sm" id="inputs">
-            <InputGroup.Text id="basic-addon1">Threshold</InputGroup.Text>
-            <FormControl
-              aria-label="lower"
-              aria-describedby="basic-addon1"
-              defaultValue={inputValue}
-              onBlur={handleInputChange}
-            />
-            <Button variant="outline-secondary" id="button-addon2" onClick={handleButtonOnClick}>
-              See Spike
-            </Button>
-          </InputGroup>
-        </Col>
-      </Row>
       <div className="eeg-title">
         <div>EEGs </div>
         <div className="referenceDIV" id="null"></div>

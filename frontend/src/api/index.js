1b6a89dd9c69c1fee89b4a0d34fbb6c71a35f83f
Juan Trelles <juantrelles@gmail.com>
2023-05-05 14:24:33 -0500

    move api to env

---
+++ frontend/src/api/index.js
@@ -0,0 +1,42 @@
+import { run } from "./client";
+
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
+
+const emptyRequestData = {
+  data: undefined,
+  token: undefined,
+  params: undefined,
+};
+
+export const fetchEEGperPatient = async (
+  patientId,
+  sampleId,
+  startMs,
+  numRecords,
+  electrodes // List of int
+) => {
+  const strElectrodes = electrodes.join(",");
+  const url = `${API_ENDPOINT}/patient/${patientId}/eeg/${sampleId}/${startMs}/${numRecords}/${strElectrodes}`;
+
+  try {
+    const { data } = await run(url, "get", emptyRequestData);
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error };
+  }
+};
+
+export const fetchSimilarRegions = async (
+  patientId,
+  sampleId,
+  eventId,
+  numNeighbors
+) => {
+  const url = `http://127.0.0.1:5000//patient/${patientId}/similar/${sampleId}/${eventId}/${numNeighbors}`;
+  try {
+    const { data } = await run(url, "get", emptyRequestData);
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error };
+  }
+};
--- frontend/src/components/eeg-data-viewer/EEGDataContainer.js
+++ frontend/src/components/eeg-data-viewer/EEGDataContainer.js
@@ -1,73 +1,74 @@
 import { EEGDataViewer } from "./EEGDataViewer";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import { fetchEEGperPatient } from "../../api";
 
 export const EEGDataContainer = ({
+  allEventData,
+  patient,
+  selectedEventRange,
+  eegPanelRange,
+  electrodeListEventWindow,
+}) => {
+  const [eegData, seteegData] = useState(null);
+  const [dataViewer, setDataViewer] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data, error } = fetchEEGperPatient(
+        patient.id,
+        patient.sample,
+        eegPanelRange[0],
+        500,
+        electrodeListEventWindow
+      );
+      // TODO: if error do something
+      seteegData(data);
+
+      const filteredData = allEventData[patient.sample].filter((el) =>
+        el.time.some(
+          (t) => t >= selectedEventRange[0] && t <= selectedEventRange[1]
+        )
+      );
+
+      const electrodeList = [
+        ...new Set(
+          filteredData.reduce((acc, cur) => acc.concat(cur.electrode), [])
+        ),
+      ];
+
+      const eventList = filteredData.map((el) => el.index);
+
+      setDataViewer({
+        eventList: eventList,
+        electrodeList: electrodeList,
+      });
+    }
+    fetchData();
+  }, [
     allEventData,
+    eegPanelRange,
+    electrodeListEventWindow,
     patient,
     selectedEventRange,
-    eegPanelRange,
-    electrodeListEventWindow
-}) => {
+  ]);
 
-    const [eegData, seteegData] = useState(null);
-    const [dataViewer, setDataViewer] = useState(null);
+  const eegList = eegData ? Object.keys(eegData.eeg).map(Number).sort() : [];
 
-    // console.log("eegPanelRange", eegPanelRange, "electrodeListEventWindow", electrodeListEventWindow, "patient", patient)
-    // console.log(eegPanelRange)
-    useEffect(() => {
-        // console.log("fetching eeg data", eegPanelRange)
-        const url = `http://127.0.0.1:5000//patient/${patient.id}/eeg/${patient.sample}/${eegPanelRange[0]}/${499}/${electrodeListEventWindow.join(",")}`
-
-        // console.log(url)
-        async function fetchData() {
-            try {
-                const response = await axios.get(url);
-                // console.log("response", response)
-                seteegData(response.data);
-
-                const filteredData = allEventData[patient.sample]
-                    .filter((el) => el.time.some(t => t >= selectedEventRange[0] && t <= selectedEventRange[1]))
-
-                const electrodeList = [...new Set(filteredData.reduce((acc, cur) => acc.concat(cur.electrode), []))];
-
-                const eventList = filteredData.map((el) => el.index);
-
-
-                setDataViewer({
-                    eventList: eventList,
-                    electrodeList: electrodeList
-                })
-
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchData();
-
-    }, [allEventData, eegPanelRange, electrodeListEventWindow, patient, selectedEventRange])
-
-    // console.log(eegData)
-    // console.log(dataViewer)
-
-    const eegList = eegData ? Object.keys(eegData.eeg).map(Number).sort() : [];
-
-    return (
-        <>
-            {
-                (electrodeListEventWindow.length === eegList.length && electrodeListEventWindow.sort().every((value, index) => value === eegList[index])) ? (
-                    <EEGDataViewer
-                        eegData={eegData}
-                        eventList={dataViewer.eventList}
-                        electrodeListEventWindow={electrodeListEventWindow}
-                        electrodeList={dataViewer.electrodeList}
-                        xTicks={eegPanelRange}
-                        selectedEventRange={selectedEventRange}
-                    />
-                ) : null
-            }
-        </>
-
-    )
-}
-
+  return (
+    <>
+      {electrodeListEventWindow.length === eegList.length &&
+      electrodeListEventWindow
+        .sort()
+        .every((value, index) => value === eegList[index]) ? (
+        <EEGDataViewer
+          eegData={eegData}
+          eventList={dataViewer.eventList}
+          electrodeListEventWindow={electrodeListEventWindow}
+          electrodeList={dataViewer.electrodeList}
+          xTicks={eegPanelRange}
+          selectedEventRange={selectedEventRange}
+        />
+      ) : null}
+    </>
+  );
+};

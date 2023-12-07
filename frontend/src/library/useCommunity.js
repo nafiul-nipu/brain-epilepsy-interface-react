91ec91cf5c44988804c94873d7497299f02d0de2
nafiulnipu <mnipu2@uic.edu>
2023-12-07 13:21:01 -0600

    dynamic community fetched

--- frontend/src/library/useCommunity.js
+++ frontend/src/library/useCommunity.js
@@ -13,8 +13,8 @@ export const useCommunity = ({
         // console.log('useSample', patientID, sampleName, range)
         if (patientID && sampleName && range) {
             // console.log("inside", patientID, sampleName, range)
-            // const url = `https://raw.githubusercontent.com/nafiul-nipu/brain-epilepsy-interface-react/dynamic_community/frontend/src/data/communities/${patientID}_communities.json`
-            const url = `https://raw.githubusercontent.com/nafiul-nipu/brain-epilepsy-interface-react/dynamic_community/frontend/src/data/communities/${patientID}_static_communities.json`
+            const url = `https://raw.githubusercontent.com/nafiul-nipu/brain-epilepsy-interface-react/dynamic_community/frontend/src/data/communities/${patientID}_communities.json`
+            // const url = `https://raw.githubusercontent.com/nafiul-nipu/brain-epilepsy-interface-react/dynamic_community/frontend/src/data/communities/${patientID}_static_communities.json`
             // console.log(url)
             json(url).then(jdata => {
                 // console.log(jdata)

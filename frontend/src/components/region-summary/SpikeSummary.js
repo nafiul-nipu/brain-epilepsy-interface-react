d34ce61daa803220c191d1e14943c196568bf6d0
nafiulnipu <mnipu2@uic.edu>
2024-03-28 01:44:58 -0500

    color added

--- frontend/src/components/region-summary/SpikeSummary.js
+++ frontend/src/components/region-summary/SpikeSummary.js
@@ -17,15 +17,15 @@ const seizure = {
 const electrodeColorList = [
     '#007ed3',
     '#FF004F',
-    '#9F8170',
     '#9400D3',
     '#FFC40C',
     '#59260B',
     '#FE4EDA',
-    '#40E0D0',
     '#FF4F00',
+    '#C19A6B',
+    '#9F8170',
     '#006D6F',
-    '#C19A6B'
+    '#40E0D0',
 ]
 
 
